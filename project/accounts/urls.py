9fb80e09d13d080132064aa04c98981109d94505
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-01-05 16:43:29 +0100

    created base.html

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from accounts import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('', views.test_base_html),
 ]
