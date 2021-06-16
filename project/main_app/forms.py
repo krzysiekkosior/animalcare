c0afced70aa8893ec65f13c16b80835049647ec6
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-06-16 16:15:24 +0200

    Created Case and CasePhoto forms

---
+++ project/main_app/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from main_app.models import Case, CasePhoto
+
+
+class CaseForm(forms.ModelForm):
+    class Meta:
+        model = Case
+        fields = ('type', 'place', 'description')
+        labels = {
+            'type': 'Rodzaj zgłoszenia',
+            'place': 'Lokalizacja',
+            'description': 'Opis'
+        }
+        widgets = {
+            'description': forms.Textarea(attrs={'rows': 8, 'cols': 70}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(CaseForm, self).__init__(*args, **kwargs)
+        self.fields['description'].widget.attrs['placeholder'] = 'Podaj jak najwięcej szczegółów: wygląd i zachowanie zwierzęcia, ' \
+                                                                 'godzinę i datę znalezenia/zaginięcia itp.'
+
+
+class PhotoForm(forms.Form):
+    photos = forms.ImageField(widget=forms.ClearableFileInput(attrs={'multiple': True}),
+                             required=False, label='Dodaj zdjęcia (opcjonalnie)')
