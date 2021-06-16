f5d7c402d488fd9e227706d62a78268157f8db0e
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-06-16 17:07:20 +0200

    test for adding case

--- project/main_app/tests.py
+++ project/main_app/tests.py
@@ -1,12 +1,35 @@
 import pytest
 
+from main_app.models import Case
+
+
 @pytest.mark.django_db
 def test_landing_page_url(client):
     response = client.get('/')
     assert response.status_code == 200
+
 
 @pytest.mark.django_db
 def test_user_profile_url(client, user):
     client.login(username='user', password='pass')
     response = client.get('/accounts/profile/')
     assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_case_url(client):
+    response = client.get('/add-case/')
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_add_case(client):
+    pre_add_cases_amount = Case.objects.count()
+    context = {
+        'type': '0',
+        'place': 'place',
+        'description': 'description'
+    }
+    client.post('/add-case/', context)
+    after_add_cases_amount = Case.objects.count()
+    assert after_add_cases_amount == pre_add_cases_amount + 1
