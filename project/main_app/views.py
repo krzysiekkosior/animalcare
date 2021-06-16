6b92d8f4e40de69e17bbf02f17557204a7d24d86
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-06-16 16:17:51 +0200

    feature: adding cases (with photos) + css for form

--- project/main_app/views.py
+++ project/main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
-from main_app.models import Case
+from main_app.forms import CaseForm, PhotoForm
+from main_app.models import Case, CasePhoto
 
 
 def main_page(request):
@@ -12,3 +14,38 @@ def cases_list(request):
         'cases': cases
     }
     return render(request, 'cases_list.html', ctx)
+
+
+class AddCaseView(View):
+
+    def get(self, request):
+        case_form = CaseForm()
+        photo_form = PhotoForm()
+        ctx = {
+            'caseForm': case_form,
+            'photoForm': photo_form
+        }
+        return render(request, 'add_case.html', ctx)
+
+    def post(self, request):
+        case_form = CaseForm(request.POST)
+        if case_form.is_valid():
+            user = request.user
+            if user.is_authenticated:
+                case = Case(user=user, **case_form.cleaned_data)
+                case.save()
+            else:
+                case = Case(**case_form.cleaned_data)
+                case.save()
+            photos = request.FILES.getlist('photos')
+            if len(photos) != 0:
+                for photo in photos:
+                    CasePhoto.objects.create(photo=photo, case=case)
+            return redirect('cases_list')
+
+        photo_form = PhotoForm()
+        ctx = {
+            'caseForm': case_form,
+            'photoForm': photo_form
+        }
+        return render(request, 'add_case.html', ctx)
--- project/project/settings.py
+++ project/project/settings.py
@@ -17,7 +17,6 @@ import my_passwords
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -28,7 +27,6 @@ SECRET_KEY = 'fz5(%2aeg9=y(_5q_f1t1(jizgv$h9ungsc!b35u7819+on7-%'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -86,7 +83,6 @@ DATABASES = {
         'PASSWORD': my_passwords.database_password
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -106,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -119,7 +114,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
@@ -142,3 +136,4 @@ LOGOUT_REDIRECT_URL = '/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
