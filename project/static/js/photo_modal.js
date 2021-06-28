1ec7be92fa1469607cb36ad697b0274469354e98
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-06-28 16:11:49 +0200

    Added closing photo-modal with escape-key

--- project/static/js/photo_modal.js
+++ project/static/js/photo_modal.js
@@ -1,6 +1,7 @@
 let modal = document.querySelector(".modal")
 let images = document.querySelectorAll("#caseImg");
 let modalImg = document.getElementById("modalId");
+let body = document.querySelector("body");
 
 for (let img of images) {
     img.addEventListener("click", e => {
@@ -11,3 +12,9 @@ for (let img of images) {
 
 let closeButton = document.querySelector(".close");
 closeButton.addEventListener("click", () => modal.style.display = "none");
+
+body.addEventListener("keydown", e => {
+    if (e.key === "Escape") {
+        modal.style.display = "none";
+    }
+})
