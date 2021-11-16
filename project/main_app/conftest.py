33ef90c73fdff40dd7a6493a0e43fc9be1506de1
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-11-16 15:33:27 +0100

    Added 'case' fixture

--- project/main_app/conftest.py
+++ project/main_app/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from django.test import Client
 
 from accounts.models import CustomUser
+from main_app.models import Case
 
 
 @pytest.fixture
@@ -19,3 +20,13 @@ def user():
     user.set_password('pass')
     user.save()
     return user
+
+@pytest.fixture
+def case(user):
+    case = Case.objects.create(
+        user=user,
+        type=0,
+        place='next to Lorem Ipsum',
+        description='big, dappled cow',
+    )
+    return case
