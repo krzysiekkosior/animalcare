e466ab923779208497a1504ef882898d71471520
Krzysiek Kosior <krzysiek.kosior@gmail.com>
2021-11-16 15:33:57 +0100

    Added test: closing case as user (without permission to close task)

--- project/main_app/tests.py
+++ project/main_app/tests.py
@@ -33,3 +33,9 @@ def test_add_case(client):
     client.post('/add-case/', context)
     after_add_cases_amount = Case.objects.count()
     assert after_add_cases_amount == pre_add_cases_amount + 1
+
+@pytest.mark.django_db
+def test_close_case_as_user(client, user, case):
+    client.login(username='user', password='pass')
+    response = client.get(f'/case/{case.pk}/close/')
+    assert response.status_code == 403
